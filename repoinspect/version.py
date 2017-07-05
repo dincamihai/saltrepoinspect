724848e72994b3470cdd446f71ad0e6f6bdd8a44
Mihai Dinca <mdinca@suse.de>
2017-07-05 13:45:43 +0200

    Add get-salt-version-from-repo feature

---
+++ repoinspect/version.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_salt_version_in_repo(repo_url):
+    resp = requests.get("{0}/x86_64".format(repo_url))
+    resp.raise_for_status()
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    ex = re.compile(r'^salt-(?P<version>[0-9a-z-\.]+)-(?P<build>[0-9\.]+).x86_64.rpm')
+    salt = soup.find('a', href=ex)
+    match = ex.match(salt.text)
+    return match.groupdict()['version']
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='saltrepoinspect',
+      version='0.1',
+      description='Tools to inspect a salt rpm repository',
+      url='https://github.com/dincamihai/saltrepoinspect.git',
+      author='Mihai Dincă',
+      author_email='dincamihai@gmail.com',
+      license='MIT',
+      packages=['saltrepoinspect'],
+      zip_safe=False)
